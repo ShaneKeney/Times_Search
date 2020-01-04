a7f260b3ccd79cdd286fcfaad960b95aba85682e
Shane Keney <shane@spikeball.com>
2020-01-04 13:08:29 -0500

    added baseURL for API endpoint

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,15 @@
-var apiKey = "K5yXDyKWAGaa7d3N8d3Z1vBxZvfp3u92";
+var apiKey = "api-key=K5yXDyKWAGaa7d3N8d3Z1vBxZvfp3u92";
+var baseURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+ 
+function queryAll() {
+    let searchTermPlaceholder = "Obama";
+    let numberOfRecords = 5;
+    let startYear = "2009";
+    let endYear = "2010";
+
+    $.ajax({
+
+    }).then(function(response) {
+
+    });
+}
