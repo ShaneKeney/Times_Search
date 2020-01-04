225f522bd8fda62bc32193daea3ed2a44db11033
Shane Keney <shane@spikeball.com>
2020-01-04 13:37:12 -0500

    - document ready added

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,10 @@
 var apiKey = "api-key=K5yXDyKWAGaa7d3N8d3Z1vBxZvfp3u92";
 var baseURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
 
+$(document).ready(() => {
+    
+});
+
 function queryAll() {
     let searchTermPlaceholder = "Obama";
     let numberOfRecords = 5;
