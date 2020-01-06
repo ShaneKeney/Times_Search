aa8f7645db670fb369c4caa0d1e8652c2b77bb9c
Shane Keney <shane@spikeball.com>
2020-01-05 19:05:26 -0500

    - created renderArticleInfo function * create the article element dynam on page - finished queryAll function - added ids to html elements for identification - added template for article element - added styles for any necessary elements

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,29 @@
 var apiKey = "api-key=K5yXDyKWAGaa7d3N8d3Z1vBxZvfp3u92";
 var baseURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
 
+var articleContainer = $(".articleContainer").clone();
+
 $(document).ready(function() {
+    //remove template (doing this for speed not good practice)
+    $(".articleContainer").remove();
+    
     //add event listener for search button
     $("#searchArticles").on("click", function(event) {
         event.preventDefault();
+        clearCurrentArticles();
 
         //do nothing if there is no search term
         if($("#search-term").val() === "") return;
 
         if($("#start-year").val() !== "" && $("#end-year").val() !== "") {
             //do queryAll search
-            console.log('queryAll');
             queryAll();
         } else if($("#start-year").val() !== "") {
             //do single start year query
-            console.log("start year only");
             //added the function call for strtYear only
             withStrtYear();
         } else if($("#end-year").val() !== "") {
             //do single end year query
-            console.log("end year only");
             ////added the function call for endYear only
             withEndYear();
         } else {
@@ -28,21 +31,53 @@ $(document).ready(function() {
             console.log("search term alone");
         }
     });
+
+    $("#clearSearchHistory").on("click", function(event) {
+        event.preventDefault();
+        clearCurrentArticles(event);
+    });
+
+
 });
 
+function clearCurrentArticles() {
+    $(".articleContainer").remove();
+}
+
 function queryAll() {
-    let searchTermPlaceholder = "Obama";
-    let numberOfRecords = 5;
-    let startYear = "2009";
-    let endYear = "2010";
+    let searchTerm = $("#search-term").val().trim();
+    let numberOfRecords = $("#numberOfRecords").val();
+    let startYear = $("#start-year").val().trim();
+    let endYear = $("#end-year").val().trim();
 
-    let queryBuilder = `${baseURL}q=${searchTermPlaceholder}&begin_date=${startYear}1231&end_date=${endYear}0101&${apiKey}`;
+    let queryBuilder = `${baseURL}q=${searchTerm}&begin_date=${startYear}1231&end_date=${endYear}0101&${apiKey}`;
 
+    let articles = [];
     $.ajax({
         url: queryBuilder,
         method: 'GET'
     }).then(function(response) {
-        console.log(response);
+        //push proper number of articles into articles array
+        for(let i=0; i<numberOfRecords; i++) {
+            articles.push(response.response.docs[i]);
+        }
+
+        renderArticleInfo(articles);
+    });
+}
+
+//Use this function to render elements to the top articles
+function renderArticleInfo(articles) {
+    articles.forEach((item, index) => {
+        let addArticleInfo = articleContainer.clone();
+
+        addArticleInfo.find(".articleIndicator").text(index+1);
+        addArticleInfo.find(".headerText").text(item.headline.main);
+        addArticleInfo.find(".dateTime").text(item.pub_date);
+        addArticleInfo.find(".mediaType").text(`Media Type: ${item.type_of_material}`);
+        addArticleInfo.find(".abstract").text(item.abstract);
+
+        $("#top-articles").append(addArticleInfo);
     });
 }
 
